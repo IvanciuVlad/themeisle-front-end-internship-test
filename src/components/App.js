5c2f1c61a783c2365eaf5e0d12b3be7cd3f6563d
IvanciuVlad <ivanciu.vlad@yahoo.com>
2020-12-10 19:33:21 +0200

    Card content almost completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,47 @@
 import React from "react";
 import getSlides from "../apis/GetSlides";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircle} from "@fortawesome/free-solid-svg-icons";
 import '../css/styling.scss';
 
+function createDate(date) {
+    console.log(date);
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const dateTime = new Date(date*1000);
+    console.log(dateTime.toLocaleDateString("en-US"));
+    return months[dateTime.getMonth()] + " " +  dateTime.getDate() + ", " + dateTime.getFullYear();
+}
+
+
 function App(props) {
-    /*    const [resources, setResources] = useState([]);
-
-        useEffect(() => {
-            fetch('https://my-json-server.typicode.com/Codeinwp/front-end-internship-api/posts',
-                {
-                    method: "GET"
-                }).then(res => res.json())
-                .then(response => {
-                    console.log(response);
-                    setResources(response);
-                }).catch(error => console.log(error));
-        });*/
-
     const slides = getSlides();
 
     if (slides) {
         return (
             <div>
-                Hello word
                 {slides.map(slide =>
-                    <li key={slide.id}>
-                        <p>{slide.title}</p>
+                    <div className="card" key={slide.id}>
                         <img src={slide.thumbnail.small} alt="ceva poza de fundal"/>
-                    </li>
+
+                        <div className="container">
+                            <li>
+                                <FontAwesomeIcon icon={faCircle} className="circles" style={{color:'#0099cc'}} />
+                                <FontAwesomeIcon icon={faCircle} className="circles" style={{color:'#cccc00'}} />
+                            </li>
+                            <li>
+                                <h2 className="card-title">{slide.title}</h2>
+                            </li>
+                            <li className="card-content">
+                                <p>{slide.content}</p>
+                            </li>
+                            <li>
+                                <div className="two-column-div">
+                                    <p className="slide-bottom-content">{slide.author.name} - {slide.author.role}</p>
+                                    <p className="slide-bottom-content" style={{textAlign:"right"}}>{createDate(slide.date)}</p>
+                                </div>
+                            </li>
+                        </div>
+                    </div>
                 )}
             </div>
         );
